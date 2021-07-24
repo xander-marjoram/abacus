bfbf18b6873d36a80e4414f8999e2a443dfb1c7d
Xander Marjoram <xander.marjoram@outlook.com>
2021-07-24 12:29:55 +0100

    Extract Question component

---
+++ components/Question.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="question-container">
+        <div class="question">
+            <div>
+                <span>{{ firstNumber }}</span>
+                <span>{{ operator }}</span>
+                <span>{{ secondNumber }}</span>
+            </div>
+            <span>=</span>
+            <div>
+                <input
+                    v-model="guess"
+                    type="number"
+                    min="0"
+                    step="1"
+                    @keydown.enter="checkGuess">
+            </div>
+        </div>
+        <div class="result-container">
+            <span class="result">
+                {{ resultText }}
+            </span>
+            <button type="button" @click="checkGuess">
+                Check
+            </button>
+            <button type="button" @click="generateNewNumbers">
+                Reset
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data: () => ({
+        answer: 0,
+        firstNumber: 0,
+        guess: null,
+        operator: '+',
+        resultText: '',
+        secondNumber: 0
+    }),
+
+    computed: {
+        ...mapState('settings', [
+            'digits'
+        ])
+    },
+
+    watch: {
+        digits (newDigits) {
+            if (this.answer >= 10 ** newDigits) {
+                this.generateNewNumbers();
+            }
+        }
+    },
+
+    created () {
+        this.generateNewNumbers();
+    },
+
+    methods: {
+        checkGuess () {
+            if (parseInt(this.guess) === this.answer) {
+                this.resultText = 'Correct!';
+            } else {
+                this.resultText = 'Incorrect';
+            }
+        },
+
+        generateNewNumbers () {
+            this.resetResults();
+            this.answer = this.getRandomNumberBetween(2, 10 ** this.digits);
+            this.firstNumber = this.getRandomNumberBetween(1, this.answer);
+            this.secondNumber = this.answer - this.firstNumber;
+        },
+
+        getRandomNumberBetween (min, max) {
+            const diff = max - min;
+            const random = Math.random() * diff;
+            return min + Math.floor(random);
+        },
+
+        resetResults () {
+            this.guess = null;
+            this.resultText = '';
+        }
+    }
+};
+</script>
+
+<style>
+.question-container {
+    margin: auto;
+    width: 95%;
+    max-width: 500px;
+}
+
+.question {
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    justify-items: center;
+    justify-content: center;
+}
+
+.result-container {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.result {
+    min-height: 50px;
+    grid-column: 1 / 3;
+}
+
+span {
+    font-size: 36px;
+    text-align: center;
+    margin: 0 8px;
+}
+
+input {
+    height: 44px;
+    font-size: 32px;
+    width: 140px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,105 +1,21 @@
 <template>
     <div>
         <settings />
-
-        <abacus
-            :digits="digits"
-            :show-numbers="true"
-            :sound-enabled="true" />
-
-        <div class="question-container">
-            <div class="question">
-                <div>
-                    <span>{{ firstNumber }}</span>
-                    <span>+</span>
-                    <span>{{ secondNumber }}</span>
-                </div>
-                <span>=</span>
-                <div>
-                    <input
-                        v-model="answer"
-                        type="number"
-                        min="0"
-                        step="1"
-                        @keydown.enter="checkAnswer">
-                    <span>
-                        ?
-                    </span>
-                </div>
-            </div>
-            <div class="result-container">
-                <span class="result">{{ resultText }}</span>
-                <button type="button" @click="checkAnswer">
-                    Check
-                </button>
-                <button type="button" @click="generateNewNumbers">
-                    Reset
-                </button>
-            </div>
-        </div>
+        <abacus />
+        <question />
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
 import Abacus from '@/components/Abacus.vue';
+import Question from '@/components/Question.vue';
 import Settings from '@/components/Settings.vue';
 
 export default {
     components: {
         Abacus,
+        Question,
         Settings
-    },
-
-    data: () => ({
-        answer: null,
-        firstNumber: 0,
-        resultText: '',
-        secondNumber: 0,
-        total: 0
-    }),
-
-    computed: {
-        ...mapState('settings', [
-            'digits'
-        ])
-    },
-
-    watch: {
-        digits (newDigits) {
-            if (this.total > 10 ** newDigits) {
-                this.generateNewNumbers();
-            }
-        }
-    },
-
-    created () {
-        this.generateNewNumbers();
-    },
-
-    methods: {
-        checkAnswer () {
-            if (parseInt(this.answer) === this.total) {
-                this.resultText = 'Correct!';
-            } else {
-                this.resultText = 'Incorrect';
-            }
-        },
-
-        generateNewNumbers () {
-            this.answer = null;
-            this.resultText = '';
-            this.total = this.getRandomNumberBetween(2, 10 ** this.digits);
-            this.firstNumber = this.getRandomNumberBetween(1, this.total);
-            this.secondNumber = this.total - this.firstNumber;
-        },
-
-        getRandomNumberBetween (min, max) {
-            const diff = max - min;
-            const random = Math.random() * diff;
-            return min + Math.floor(random);
-        }
     }
 };
 </script>
@@ -119,42 +35,5 @@ button:focus {
 
 button:active {
     background-color: #ccc
-}
-
-.question-container {
-    margin: auto;
-    width: 95%;
-    max-width: 500px;
-}
-
-.question {
-    width: 100%;
-    display: flex;
-    flex-flow: row wrap;
-    justify-items: center;
-    justify-content: center;
-}
-
-.result-container {
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-}
-
-.result {
-    min-height: 50px;
-    grid-column: 1 / 3;
-}
-
-span {
-    font-size: 36px;
-    text-align: center;
-    margin: 0 8px;
-}
-
-input {
-    height: 44px;
-    font-size: 32px;
-    width: 140px;
 }
 </style>
